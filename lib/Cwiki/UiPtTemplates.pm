b326c606cd63718f523e154f1e3f78b28b3257d3
Colin Hogben <perl@pythontech.co.uk>
2006-12-06 09:17:29 +0000

    HTML:xyz for pre-formatted HTML fragments

--- lib/Cwiki/UiPtTemplates.pm
+++ lib/Cwiki/UiPtTemplates.pm
@@ -1,5 +1,5 @@
 #=======================================================================
-#	$Id: UiPtTemplates.pm,v 1.1 2006/03/21 14:11:39 pythontech Exp $
+#	$Id: UiPtTemplates.pm,v 1.2 2006/12/06 09:17:29 pythontech Exp $
 #	Presentation - templates
 #	Copyright (C) 2005  Python Technology Limited
 #
@@ -69,7 +69,7 @@ sub links {
     my($self, @topics) = @_;
     my @links = map {
 	{
-	    ltopic => $_,
+	    'HTML:ltopic' => $::wiki->fmt->topicHtml($_),
 	    lurl => $::wiki->server->url('view', Topic => $_),
 	}
     } sort @topics;
@@ -80,7 +80,7 @@ sub search {
     my($self, $search, @topics) = @_;
     my @links = map {
 	{
-	    ltopic => $_,
+	    'HTML:ltopic' => $::wiki->fmt->topicHtml($_),
 	    lurl => $::wiki->server->url('view', Topic => $_),
 	}
     } sort @topics;
@@ -111,7 +111,7 @@ sub _common {
 	$moduser = $data->{'logname'};
     }
     my $env = {
-	topic => $::topic,
+	'HTML:topic' => $::wiki->fmt->topicHtml($::topic),
 	moddate => $moddate,
 	moduser => $moduser,
 	method => $::method,
@@ -126,6 +126,8 @@ sub _common {
 	appendurl => $::wiki->server->url('append'),
 	'HTML:appendfields' => $::wiki->server->fields('append'),
 	askrenameurl => $::wiki->server->url('askrename'),
+	renameurl => $::wiki->server->url('rename'),
+	'HTML:renamefields' => $::wiki->server->fields('rename'),
 	linksurl => $::wiki->server->url('links'),
 	searchurl => $::wiki->server->url('search'),
 	'HTML:searchfields' => $::wiki->server->fields('search'),
@@ -133,10 +135,40 @@ sub _common {
     };
     (my $tmpl = $self->{'filespec'}) =~ s!%s!$tmpl!e;
     my $html = $self->{'templater'}->process_file($tmpl,
+						  $self,
 						  $env,
 						  @vars,
 						  $self->{'global'});
     return $html;
+}
+
+#-----------------------------------------------------------------------
+#	Queries for common template items
+#-----------------------------------------------------------------------
+sub GET {
+    my($self, $var,@rest) = @_;
+    if (my($method) = $var =~ m!^(edit|save|editappend|append|askrename|rename|search|links|latex)url$!) {
+	my $url = $::wiki->server->url($method);
+	return $url;
+    } elsif (my($method) = $var =~ m!^url:(\w+)$!) {
+	my $url = $::wiki->server->url($method);
+	return $url;
+    } else {
+	return undef;
+    }
+}
+
+sub HTML {
+    my($self, $var,@rest) = @_;
+    if (my($method) = $var =~ m!^(save|append|rename|search)fields$!) {
+	my $html = $::wiki->server->fields($method);
+	return $html;
+    } elsif (my($method) = $var =~ m!^hidden:(\w+)$!) {
+	my $html = $::wiki->server->fields($method);
+	return $html;
+    } else {
+	return undef;
+    }
 }
 
 1;
